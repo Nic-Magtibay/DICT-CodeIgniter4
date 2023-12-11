bcfc5db0fb547e0b30b4fb1e904cdd2994a3150c
Nic-Magtibay <nmmagtibay@up.edu.ph>
2023-12-11 14:39:34 +0800

    Added Routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,15 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+$routes->setDefaultNamespace('App\Controller');
+$routes->setDefaultController('Home');
+$routes->setDefaultMethod('index');
+$routes->setTranslateURIDashes(false);
+$routes->set404Override();
+
 $routes->get('/', 'Home::index');
+//pangalan nung route-controller
+$routes->resource('offices',['controller'=>'OfficeController','except'=>'new,edit']);
+$routes->resource('tickets',['controller'=>'TicketController','except'=>'new,edit']);
 
 service('auth')->routes($routes);
